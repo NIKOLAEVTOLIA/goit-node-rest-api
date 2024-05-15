c86addb0fa30160c8ea132f0aa3ba50d0626ce98
NIKOLAEVTOLIA <140529562+NIKOLAEVTOLIA@users.noreply.github.com>
2024-05-16 01:28:49 +0300

    fix ID Validation

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -33,18 +33,21 @@ export const getOneContact = [
   },
 ];
 
-export const deleteContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const removedContact = await contactsService.removeContact(id);
-    if (!removedContact) {
-      throw HttpError(404, "Not found");
+export const deleteContact = [
+  validID,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const removedContact = await contactsService.removeContact(id);
+      if (!removedContact) {
+        throw HttpError(404, "Not found");
+      }
+      res.status(200).json(removedContact);
+    } catch (err) {
+      next(err);
     }
-    res.status(200).json(removedContact);
-  } catch (err) {
-    next(err);
-  }
-};
+  },
+];
 
 export const createContact = [
   validateBody(createContactSchema),
@@ -59,44 +62,48 @@ export const createContact = [
   },
 ];
 
-export const updateContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { name, email, phone } = req.body;
+export const updateContact = [
+  validID,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { name, email, phone } = req.body;
 
-    if (!name && !email && !phone) {
-      throw HttpError(400, "Body must have at least one field");
+      if (!name && !email && !phone) {
+        throw HttpError(400, "Body must have at least one field");
+      }
+
+      const currentContact = await contactsService.getContactById(id);
+
+      if (!currentContact) {
+        throw HttpError(404, "Not found");
+      }
+
+      const updFields = {
+        name: name || currentContact.name,
+        email: email || currentContact.email,
+        phone: phone || currentContact.phone,
+      };
+
+      const validationResult = updateContactSchema.validate(updFields);
+      if (validationResult.error) {
+        throw HttpError(400, validationResult.error.message);
+      }
+
+      const updatedContact = await contactsService.updateContact(id, updFields);
+      if (!updatedContact) {
+        throw HttpError(404, "Not found");
+      }
+
+      res.status(200).json(updatedContact);
+    } catch (err) {
+      next(err);
     }
-
-    const currentContact = await contactsService.getContactById(id);
-
-    if (!currentContact) {
-      throw HttpError(404, "Not found");
-    }
-
-    const updFields = {
-      name: name || currentContact.name,
-      email: email || currentContact.email,
-      phone: phone || currentContact.phone,
-    };
-
-    const validationResult = updateContactSchema.validate(updFields);
-    if (validationResult.error) {
-      throw HttpError(400, validationResult.error.message);
-    }
-
-    const updatedContact = await contactsService.updateContact(id, updFields);
-    if (!updatedContact) {
-      throw HttpError(404, "Not found");
-    }
-
-    res.status(200).json(updatedContact);
-  } catch (err) {
-    next(err);
-  }
-};
+  },
+];
 
 export const updateStatusContact = [
+  validID,
   validateBody(updStatusSchema),
   async (req, res, next) => {
     try {
